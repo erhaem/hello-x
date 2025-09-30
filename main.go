9e0ac2579bcb7ce0851e50c454a41a442be1c03e
erhaem <jurig.blogger@gmail.com>
2025-09-30 22:19:12 +0700

    feat: implement http server

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now().Unix()
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	response := fmt.Sprintf(`{"message": "hello, %s","time":"%d"}`, name, now)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, response)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("server running on port :8090")
+	if err := http.ListenAndServe(":8090", nil); err != nil {
+		fmt.Printf("failed to run server: %v\n", err)
+		return
+	}
+}
